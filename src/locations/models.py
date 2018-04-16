4d4fe5e534e5f990f1929a4a2b1c86a21f9f8b72
Oleksandr Berezhnyi <oleksandr.berezhnyi@elements.nl>
2018-04-16 08:29:56 +0200

    Add models

--- src/locations/models.py
+++ src/locations/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class User(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, unique=True)
+
+class Location(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    lat = models.FloatField()
+    lng = models.FloatField()
