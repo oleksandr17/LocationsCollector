9cfecf571c317d0757688fac5898f270428c3474
Oleksandr Berezhnyi <oleksandr.berezhnyi@elements.nl>
2018-04-24 11:04:04 +0200

    Add base unit test

--- src/tests/locations/test_create_location.py
+++ src/tests/locations/test_create_location.py
@@ -1,4 +1,36 @@
 import pytest
+import json
+from django.urls import reverse
+from rest_framework import status
+from locations import models
 
-def test_numbers_3_4():
-    assert 3*4 == 12
+
+@pytest.mark.parametrize('sender_uuid, lat, lng', (
+    ['00000000-0000-0000-0000-000000000001', 10.111, 10.999],
+    ['00000000-0000-0000-0000-000000000002', 20.111, 20.999],
+    ['00000000-0000-0000-0000-000000000003', 30.111, 30.999],
+    ['00000000-0000-0000-0000-000000000004', 40.111, 40.999],
+))
+@pytest.mark.django_db
+def test_location_create(client, sender_uuid, lat, lng):
+    body = {
+        "sender_uuid": sender_uuid,
+        "lat": lat,
+        "lng": lng
+    }
+    response = client.post(reverse('locations:location_list'),
+                                    content_type='application/json',
+                                    data=json.dumps(body))
+
+    assert response.status_code == status.HTTP_201_CREATED
+
+    assert models.Sender.objects.all().count() == 1
+    sender = models.Sender.objects.first()
+    assert str(sender.uuid) == sender_uuid
+
+    assert models.Location.objects.all().count() == 1
+    location = models.Location.objects.first()
+    assert location.lat == lat
+    assert location.lng == lng
+    assert location.sender == sender
+    assert location.timestamp != None
