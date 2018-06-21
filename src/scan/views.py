18a04944991a67d4d5cbc1db1f2230c29be6761d
Oleksandr Berezhnyi <oleksandr.berezhnyi@elements.nl>
2018-06-21 08:19:32 +0200

    Add stub response for scan

--- src/scan/views.py
+++ src/scan/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class Scan(APIView):
+
+    def get(self, request, *args, **kwargs):
+        return Response({'scan': 'stub'}, status=status.HTTP_200_OK)
