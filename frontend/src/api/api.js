f567350a51019a7144249442daa09d6a6274cbb0
dinarakrasilnikova <dinarochka@gmail.com>
2018-07-05 00:14:11 +0200

    frontend part

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -30,18 +30,18 @@ export const genericErrorHandler = (error) =>
 // post location
 
 export const postLocation = (lat, lng) =>
-  axios({
-    method: 'post',
-    url: 'https://api.betterfixit.nl/v1/locations/list/',
-    data: {
-      sender_uuid: uuidv4,
-      lat,
-      lng
-    }
-  });
-  // axios.post('https://api.betterfixit.nl/v1/locations/list/', {
-  //   sender_uuid: uuidv4,
-  //   lat,
-  //   lng
+  // axios({
+  //   method: 'post',
+  //   url: 'http://127.0.0.1:8000/v1/locations/list/',
+  //   data: {
+  //     sender_uuid: uuidv4,
+  //     lat,
+  //     lng
+  //   }
   // });
+  axios.post('http://127.0.0.1:8000/v1/locations/list/', {
+    sender_uuid: "0eaed45f-af9f-40e0-902e-e5b3db35ee89",
+    lat,
+    lng
+  });
 
