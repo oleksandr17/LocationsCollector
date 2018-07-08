805af07cbb0c5ddb6d6f918836f5cdd2cdef2dd0
dinarakrasilnikova <dinarochka@gmail.com>
2018-07-09 00:15:46 +0200

    keep frontend stuff in a separate static directory

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -30,16 +30,8 @@ export const genericErrorHandler = (error) =>
 // post location
 
 export const postLocation = (lat, lng) =>
-  // axios({
-  //   method: 'post',
-  //   url: 'http://127.0.0.1:8000/v1/locations/list/',
-  //   data: {
-  //     sender_uuid: uuidv4,
-  //     lat,
-  //     lng
-  //   }
-  // });
-  axios.post('http://127.0.0.1:8000/v1/locations/list/', {
+  //axios.post('http://127.0.0.1:8000/v1/locations/list/', {
+  axios.post('/v1/locations/list/', {
     sender_uuid: "0eaed45f-af9f-40e0-902e-e5b3db35ee89",
     lat,
     lng
